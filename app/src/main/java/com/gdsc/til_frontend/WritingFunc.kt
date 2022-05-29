17c6e8f07040108c721a157ea3fc61bcdee25f48
choimyungbin <bbin852456.bbc@gmail.com>
2022-05-29 17:51:54 +0900

    okhttp 연동

--- app/src/main/java/com/gdsc/til_frontend/WritingFunc.kt
+++ app/src/main/java/com/gdsc/til_frontend/WritingFunc.kt
@@ -3,10 +3,17 @@ package com.gdsc.til_frontend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.content.Intent
-import android.view.View
-import us.feras.mdv.MarkdownView
+import android.os.Build
+import android.os.StrictMode
+import androidx.annotation.RequiresApi
 import com.gdsc.til_frontend.databinding.WritingBinding
+import com.squareup.okhttp.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.reflect.Array.get
+import java.nio.file.Paths.get
+import java.sql.DriverManager.println
+import java.time.LocalDate
 
 class WritingFunc : AppCompatActivity() {
 	private var mBinding : WritingBinding? = null
@@ -17,9 +24,15 @@ class WritingFunc : AppCompatActivity() {
 		mBinding = WritingBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 
+		if (Build.VERSION.SDK_INT > 9) {
+			val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+			StrictMode.setThreadPolicy(policy)
+		}
+
 		val markdownView = binding.markdownView
 		var uploadText :String
 		var uploadTitle: String
+		var id = 1
 		markdownView.loadMarkdown("## Input Markdown")
 		binding.applyButton.setOnClickListener {
 			uploadText = binding.editText.text.toString()
@@ -31,7 +44,8 @@ class WritingFunc : AppCompatActivity() {
 			finish()
 		}
 		binding.uploadButton.setOnClickListener {
-
+			sendPost(binding, id)
+			id+=1
 		}
 
 	}
@@ -39,5 +53,42 @@ class WritingFunc : AppCompatActivity() {
 		mBinding = null
 		super.onDestroy()
 	}
+	private fun sendPost(binding: WritingBinding, id: Int) {
+		val title = binding.editTitle.text.toString()
+		val content = binding.editText.text.toString()
+		val date = LocalDate.now().toString()
+		val JSON = MediaType.parse("application/json; charset=utf-8")
+		Log.d("upload", "upload start")
 
+//		val body =
+//			"{\n" + "\"title\": \"" + title + "\",\n\"date\":\"" + date + "\",\n\"content\":\"" + content + "\"\n}"
+		val url = "http://gdsc-knu-til.herokuapp.com/posts"
+
+		val client = OkHttpClient()
+
+		//POST 요청 객체 생성
+		val jsonInput = JSONObject()
+		try{
+			jsonInput.put("title", title)
+			jsonInput.put("date", date)
+			jsonInput.put("content", content)
+		} catch(e:JSONException){
+			e.printStackTrace();
+			return;
+		}
+		val body = RequestBody.create(JSON, jsonInput.toString())
+		Log.d("put","json input done")
+
+		val builder = Request.Builder().url(url).post(body)
+		builder.addHeader("Content-Type", "application/json; charset=utf-8")
+		val request = builder.build()
+		Log.d("build","build done")
+
+		val response = client.newCall(request).execute()
+		if (response.isSuccessful) {
+			val postId = response.body().string()
+
+			return
+		}
+	}
 }
